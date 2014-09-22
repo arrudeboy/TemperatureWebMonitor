e0f21e8924256abf3e2fe4e2da23fa5459cc66cc
arrudeboy <arturofelixchari@gmail.com>
2014-09-22 20:22:08 -0300

    Update saveEmails.php

--- webserver/emails/saveEmails.php
+++ webserver/emails/saveEmails.php
@@ -1,6 +1,6 @@
 <?php
 $emails = "";
-$HANDLE = fopen('admin_emails.config', 'w') or die ('CANT OPEN FILE');
+$HANDLE = fopen('config/admin_emails.config', 'w') or die ('CANT OPEN FILE');
 if ($_POST['email1'] != "")
 	$emails = $_POST['email1'];
 if ($_POST['email2'] != "")
