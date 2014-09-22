e98ce4db0f98f32be8a603eb0726471d7b1cb08b
arrudeboy <arturofelixchari@gmail.com>
2014-09-22 19:57:55 -0300

    Create getAdminEmails.php

---
+++ webserver/emails/getAdminEmails.php
@@ -0,0 +1,23 @@
+<?php
+$admin_email1 = "";
+$admin_email2 = "";
+$admin_email3 = "";
+$admin_email4 = "";
+$array = array();
+$file = fopen('admin_emails.config', 'r') or die ('CANT OPEN FILE');
+while(!feof($file)){
+    $line = fgets($file);
+    $array[] = $line;
+}
+if(array_key_exists(0, $array)) 
+    $admin_email1 = $array[0];
+if(array_key_exists(1, $array))        
+    $admin_email2 = $array[1];
+if(array_key_exists(2, $array))        
+    $admin_email3 = $array[2];
+if(array_key_exists(3, $array))        
+    $admin_email4 = $array[3];
+
+fclose($file);
+
+?>
