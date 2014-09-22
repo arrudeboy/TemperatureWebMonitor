08c088f1700ed0cc38e4f871e556fa10b6fe24a8
arrudeboy <arturofelixchari@gmail.com>
2014-09-22 19:56:22 -0300

    Create saveEmails.php

---
+++ webserver/emails/saveEmails.php
@@ -0,0 +1,16 @@
+<?php
+$emails = "";
+$HANDLE = fopen('admin_emails.config', 'w') or die ('CANT OPEN FILE');
+if ($_POST['email1'] != "")
+	$emails = $_POST['email1'];
+if ($_POST['email2'] != "")
+	$emails .= "\n" . $_POST['email2'];
+if ($_POST['email3'] != "")	
+	$emails .= "\n" . $_POST['email3'];
+if ($_POST['email4'] != "")
+	$emails .= "\n" . $_POST['email4'];
+	
+fwrite($HANDLE,$emails);
+fclose($HANDLE);
+		
+?>
