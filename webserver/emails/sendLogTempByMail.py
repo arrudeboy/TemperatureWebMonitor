15c51d344a47117574eddc860d8c751aca97a87d
arrudeboy <arturofelixchari@gmail.com>
2014-09-22 20:31:04 -0300

    Update sendLogTempByMail.py

--- webserver/emails/sendLogTempByMail.py
+++ webserver/emails/sendLogTempByMail.py
@@ -14,7 +14,7 @@ PASSWORD = "mypasswd"
 MAILTO = ''
 
 # open file with full path 
-with open('/mnt/sda1/temperaturewebmonitor/config/admin_emails.config') as data:
+with open('/mnt/sda1/webserver/config/admin_emails.config') as data:
     datalines = (line.rstrip('\n') for line in data)
     for line in datalines:
         MAILTO += line
