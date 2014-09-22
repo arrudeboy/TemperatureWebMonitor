040f55a4157e2054b54013e8e8837810a75a5873
arrudeboy <arturofelixchari@gmail.com>
2014-09-22 20:29:33 -0300

    Update saveLimitTempValue.php

--- webserver/cluster/saveLimitTempValue.php
+++ webserver/cluster/saveLimitTempValue.php
@@ -1,6 +1,6 @@
 <?php
 $value = $_POST['limitTemp'];
-$HANDLE = fopen('config/limitTemp.config', 'w') or die ('CANT OPEN FILE');
+$HANDLE = fopen('../config/limitTemp.config', 'w') or die ('CANT OPEN FILE');
 fwrite($HANDLE,$value);
 fclose($HANDLE);
 ?>
